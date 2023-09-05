95033795782a5c05b64c7c40e0b3019ab05f8677
KasunKVI <kkhettiarachchi10@gmail.com>
2023-09-05 12:41:56 +0530

    implement the DAOFactory class

---
+++ src/main/java/lk/ijse/dao/DAOFactory.java
@@ -0,0 +1,36 @@
+package lk.ijse.dao;
+
+import lk.ijse.dao.custom.impl.ReservationDAOImpl;
+import lk.ijse.dao.custom.impl.RoomDAOImpl;
+import lk.ijse.dao.custom.impl.StudentDAOImpl;
+
+public class DAOFactory {
+
+    private static DAOFactory daoFactory;
+
+    private DAOFactory(){}
+
+    public static DAOFactory getDaoFactory(){
+        return (daoFactory==null) ? daoFactory=new DAOFactory() : daoFactory;
+    }
+
+    public enum DAOTypes{
+        STUDENT,RESERVATION,ROOM,USER
+
+    }
+
+    public SuperDAO getDAO(DAOTypes types){
+
+        switch (types){
+            case ROOM:
+                return new RoomDAOImpl();
+            case STUDENT:
+                return new StudentDAOImpl();
+            case RESERVATION:
+                return new ReservationDAOImpl();
+            default:
+                return null;
+        }
+    }
+
+}
